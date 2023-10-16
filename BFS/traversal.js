e65461e33cc7a59e5b6d1b37bcb0a27ad0c27d25
Arun Kataria <arunkataria@Aruns-MacBook-Pro.local>
2023-10-16 22:59:57 +0530

    bfs traversal

---
+++ BFS/traversal.js
@@ -0,0 +1,58 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+}
+
+Graph.prototype.bfs = function (start) {
+  const queue = [start];
+  const result = [];
+  const visited = {};
+  let currentVertex;
+
+  visited[start] = true;
+
+  while (queue.length) {
+    currentVertex = queue.shift();
+    result.push(currentVertex);
+
+    this.adjacencyList[currentVertex].forEach((neighbor) => {
+      if (!visited[neighbor]) {
+        visited[neighbor] = true;
+        queue.push(neighbor);
+      }
+    });
+  }
+
+  return result;
+};
+
+const graph = new Graph();
+
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "D");
+graph.addEdge("C", "E");
+graph.addEdge("D", "E");
+graph.addEdge("D", "F");
+graph.addEdge("E", "F");
+
+console.log(graph.bfs("A")); // Expected output: ['A', 'B', 'C', 'D', 'E', 'F']
